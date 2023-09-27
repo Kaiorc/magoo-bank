946fc1eefb6352f3702c81ad24ff9be99178e286
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-09-27 13:29:58 -0300

    Arquivo "tsconfig.json" adicionado

--- dist/js/magoobank.js
+++ dist/js/magoobank.js
@@ -1,12 +1,13 @@
 // Define o saldo inicial da conta
-var saldo = 3000;
+let saldo = 3000;
+alert("Testando compalação do TS-");
 // Obtém o elemento HTML que exibe o saldo e atualiza seu valor
-var elementoSaldo = document.querySelector(".saldo-valor .valor");
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
 if (elementoSaldo != null) {
     elementoSaldo.textContent = saldo.toString();
 }
 // Obtém o formulário de transação e adiciona um listener de evento para quando o formulário for enviado
-var elementoFormulario = document.querySelector(".block-nova-transacao form");
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function (event) {
     event.preventDefault(); // Previne o comportamento padrão do formulário de ser enviado
     if (!elementoFormulario.checkValidity()) { // Verifica se todos os campos do formulário foram preenchidos corretamente
@@ -14,13 +15,13 @@ elementoFormulario.addEventListener("submit", function (event) {
         return;
     }
     // Obtém os valores dos campos do formulário
-    var inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
-    var inputValor = elementoFormulario.querySelector("#valor");
-    var inputData = elementoFormulario.querySelector("#data");
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
     // Converte os valores dos campos para os tipos corretos
-    var tipoTransacao = inputTipoTransacao.value;
-    var valor = inputValor.valueAsNumber;
-    var data = new Date(inputData.value);
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
     // Atualiza o saldo dependendo do tipo de transação selecionado
     if (tipoTransacao == "Depósito") {
         saldo += valor;
@@ -35,7 +36,7 @@ elementoFormulario.addEventListener("submit", function (event) {
     // Atualiza o elemento HTML que exibe o saldo com o novo valor do saldo
     elementoSaldo.textContent = saldo.toString();
     // Cria um objeto com os valores da transação e exibe-o no console
-    var novaTransacao = {
+    const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
