bfe51e36184b36d79838b16e989f26e3487c2bc1
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-09-27 14:43:15 -0300

    Arrays e type alias utilizados

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -3,3 +3,11 @@ let nome = "";
 let isPago = false;
 let qualquer = "";
 qualquer = 22;
+// Arrays
+const lista = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+const novaTransacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+};
