2e8d80faac4d67196df911e4c56a124b89fd291a
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-10-26 13:29:05 -0300

    Enum de formatos de data adicionado

--- dist/js/formatters.js
+++ dist/js/formatters.js
@@ -1,6 +1,12 @@
 function formatarMoeda(valor) {
     return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 }
-function formatarData(data) {
-    return data.toLocaleDateString("pt-BR", { weekday: "long", day: "2-digit", month: "2-digit", year: "numeric" });
+// Se não for informado um formato específico, ele vai aplicar o formato padrão
+function formatarData(data, formato = FormatoData.PADRAO) {
+    if (formato == FormatoData.DIA_SEMANA_DIA_MES_ANO) {
+        return data.toLocaleDateString("pt-BR", { weekday: "long", day: "2-digit", month: "2-digit", year: "numeric" });
+    }
+    else if (formato == FormatoData.DIA_MES) {
+        return data.toLocaleDateString("pt-br", { day: "2-digit", month: "2-digit" });
+    }
 }
