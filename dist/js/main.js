063e1bb8cf545104d8cda136e4a9ff63dbb278c5
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-11-27 15:50:57 -0300

    Arquivos reorganizados como módulos ES6+

---
+++ dist/js/main.js
@@ -0,0 +1,5 @@
+// É comum esse arquivo centralizador de importação ser nomeado "main.ts" ou "app.ts".
+// O arquivo .ts se refere ao desenvolvimento, ou seja, é temporario. No momento 
+// em que ele é executado na pasta "dist" no navegador esse arquivo precisa ser .js.
+import "./components/nova-transacao-component.js";
+import "./components/saldo-component.js";
